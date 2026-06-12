60c66cfece6868ea7bccbc6f8019aff05417048a
madhusamala-dev <madhu@trainingmug.com>
2026-06-12 14:12:54 +0530

    feat: derived methods and dtos

--- src/main/java/com/trainingmug/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/trainingmug/ecommerce/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.trainingmug.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,5 +18,13 @@ public interface CustomerRepository extends JpaRepository<Customer,Integer> {
     Derived Methods
     findBy, count, exists
      */
+    //SQL Query
+    //select * from customer where email = ?
     Optional<Customer> findByEmail(String email);
+    Optional<Customer> findDistinctByEmailAndPassword(String email, String password);
+    List<Customer> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    List<Customer> findByNameLike(String namePattern);
+    List<Customer> findByNameOrderByCreatedAtDesc(String name);
+
+
 }
